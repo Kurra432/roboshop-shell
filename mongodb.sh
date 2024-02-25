695ba505252b9957a154faba188e9633f1431098
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-02-25 12:23:25 +0530

    Mongodb service

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,11 @@
-echo -e "\e[36>>>>>>>>>>>>Setup the repo file<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>> Setup Mongo repo file <<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[36>>>>>>>>>>>>Install MongoDB<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>Install Mongo <<<<<<<<<\e[0m"
 dnf install mongodb-org -y
-echo -e "\e[36>>>>>>>>>>>>Start MongoDB <<<<<<<<<<<<<\e[0m"
-sed -e -i 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
-echo -e "\e[36>>>>>>>>>>>>Start MongoDB <<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>Edit the IP <<<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
+echo -e "\e[36m>>>>>>>>>>>>>>>Edit the IP <<<<<<<<<\e[0m"
 systemctl enable mongod
 systemctl restart mongod
 
