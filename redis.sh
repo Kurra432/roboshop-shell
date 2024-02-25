b4996582254432cc66507fe750d5006308d58ae3
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-02-25 17:17:29 +0530

    adding catalogue in forntend service

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ dnf module enable redis:remi-6.2 -y
 echo -e "\e[36m>>>>>>>>>>>>>Install Redis<<<<<<<<<<<\e[0m"
 dnf install redis -y
 echo -e "\e[36m>>>>>>>>>>>>>Update the listen address<<<<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 echo -e "\e[36m>>>>>>>>>>>>>Start the Redis service<<<<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl restart redis
