d95484e2a57a8d03e9a6410ee5bfa7cab01954dc
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-02-25 12:40:11 +0530

    Catalogue service

---
+++ catalogue.sh
@@ -0,0 +1,29 @@
+echo -e "\e[36m>>>>>>>>>>>>>>>Install Nodejs<<<<<<<<<\e[0m"
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+echo -e "\e[36m>>>>>>>>>>>>>>>Add Application user<<<<<<<<<\e[0m"
+useradd roboshop
+echo -e "\e[36m>>>>>>>>>>>>>>>Creating App Directory<<<<<<<<<\e[0m"
+rm -f /app
+mkdir /app
+echo -e "\e[36m>>>>>>>>>>>>>>>Downloading App Content<<<<<<<<<\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo -e "\e[36m>>>>>>>>>>>>>>>Unzip the content<<<<<<<<<\e[0m"
+cd /app
+unzip /tmp/catalogue.zip
+echo -e "\e[36m>>>>>>>>>>>>>>>Downloading Dependecies<<<<<<<<<\e[0m"
+ npm install
+ echo -e "\e[36m>>>>>>>>>>>>>>>Setup the Systemd service file<<<<<<<<<\e[0m"
+ cp /home/centos/catalogue.service /etc/systemd/system/catalogue.service
+ echo -e "\e[36m>>>>>>>>>>>>>>>Load the service file<<<<<<<<<\e[0m"
+ systemctl daemon-reload
+ echo -e "\e[36m>>>>>>>>>>>>>>>Copying Mongodb repo file<<<<<<<<<\e[0m"
+ cp /home/centos/mongo.repo /etc/yum.repos.d/mongo.repo
+ echo -e "\e[36m>>>>>>>>>>>>>>>Install Mongodb client<<<<<<<<<\e[0m"
+ dnf install mongodb-org-shell -y
+ echo -e "\e[36m>>>>>>>>>>>>>>> Update the MongoDB address<<<<<<<<<\e[0m"
+ mongo --host mongodb-dev.vdevops72.online </app/schema/catalogue.js
+echo -e "\e[36m>>>>>>>>>>>>>>>Start the Catalogue  service <<<<<<<<<\e[0m"
+systemctl enable catalogue
+systemctl restart catalogue
