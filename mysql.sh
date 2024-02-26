11c88bd62445e4279358e96b56e524bee811b78e
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-02-26 10:31:23 +0530

    Mysql service

---
+++ mysql.sh
@@ -0,0 +1,11 @@
+echo -e "\e[36m>>>>>>>>>>>>>>>MySQL Disable<<<<<<<<<\e[0m"
+dnf module disable mysql -y
+echo -e "\e[36m>>>>>>>>>>>>>>>Copying MySQL repo file<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mysql.repo  /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>>>>>>>>>Install MySQL <<<<<<<<<\e[0m"
+dnf install mysql-community-server -y
+echo -e "\e[36m>>>>>>>>>>>>>>>Set root Passwd for MySQL <<<<<<<<<\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
+echo -e "\e[36m>>>>>>>>>>>>>>>Start  MySQL <<<<<<<<<\e[0m"
+systemctl enable mysqld
+systemctl restart mysqld
