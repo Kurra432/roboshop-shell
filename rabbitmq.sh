beb758b528a02b20a7fac74c12e041f9ae660940
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-02-27 11:19:07 +0530

    rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -7,5 +7,5 @@ dnf install rabbitmq-server -y
 echo -e "\e[36m>>>>>>>>>>>>>>> Adding app user and passwd forRabbitMQ Service<<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 echo -e "\e[36m>>>>>>>>>>>>>>> Start RabbitMQ Service<<<<<<<<<\e[0m"
-systemctl enable rabbitmq
-systemctl restart rabbitmq
+systemctl enable rabbitmq-server
+systemctl restart rabbitmq-server
