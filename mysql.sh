86f52a44b89b28823091aae0ca6ea12e02b8ab06
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-02-28 11:13:22 +0530

    Special Variables

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+mysql_root_password=$1
 echo -e "\e[36m>>>>>>>>>>>>>>>MySQL Disable<<<<<<<<<\e[0m"
 dnf module disable mysql -y
 echo -e "\e[36m>>>>>>>>>>>>>>>Copying MySQL repo file<<<<<<<<<\e[0m"
@@ -8,7 +9,7 @@ cp ${script_path}/mysql.repo  /etc/yum.repos.d/mysql.repo
 echo -e "\e[36m>>>>>>>>>>>>>>>Install MySQL <<<<<<<<<\e[0m"
 dnf install mysql-community-server -y
 echo -e "\e[36m>>>>>>>>>>>>>>>Set root Passwd for MySQL <<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root_password}
 echo -e "\e[36m>>>>>>>>>>>>>>>Start  MySQL <<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl restart mysqld
