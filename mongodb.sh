cc3ea18a9dc9864852ba441206062323d3de41ab
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-03-05 11:49:03 +0530

    Functions

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,19 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+print_head "Copying Mongo repo file "
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+status_check_func $?
+print_head "Install MongoDB  "
+dnf install mongodb-org -y &>>$log_file
+status_check_func $?
+print_head "Update MongoDB listen Address "
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
+status_check_func $?
 
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[36m>>>>>>>>>>>>>>>Install Mongo <<<<<<<<<\e[0m"
-dnf install mongodb-org -y
-echo -e "\e[36m>>>>>>>>>>>>>>>Edit the IP <<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+print_head "Start the Mongo DB Services"
+systemctl enable mongod &>>$log_file
+systemctl restart mongod &>>$log_file
 
-echo -e "\e[36m>>>>>>>>>>>>>>>Start the Mongo DB<<<<<<<<<\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+status_check_func $?
 
