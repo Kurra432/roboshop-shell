e67221cecefad10c58c888a110b7713d8571a722
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-03-05 11:55:36 +0530

    Functions

--- redis.sh
+++ redis.sh
@@ -1,17 +1,24 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-echo -e "\e[36m>>>>>>>>>>>>>Setup the Redis repo File<<<<<<<<<<<\e[0m"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-echo -e "\e[36m>>>>>>>>>>>>>Enable the Redis  Service<<<<<<<<<<<\e[0m"
-dnf module enable redis:remi-6.2 -y
-echo -e "\e[36m>>>>>>>>>>>>>Install Redis<<<<<<<<<<<\e[0m"
-dnf install redis -y
-echo -e "\e[36m>>>>>>>>>>>>>Update the listen address<<<<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
-echo -e "\e[36m>>>>>>>>>>>>>Start the Redis service<<<<<<<<<<<\e[0m"
-systemctl enable redis
-systemctl restart redis
+print_head "Setup the Redis repo File"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$log_file
+status_check_func $?
+print_head "Enable the Redis  Service"
+dnf module enable redis:remi-6.2 -y &>>$log_file
+status_check_func $?
+print_head "Install Redis"
+dnf install redis -y &>>$log_file
+status_check_func $?
+print_head "Update the  Redis listen address"
+
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf &>>$log_file
+status_check_func $?
+
+print_head "Start the Redis service"
+systemctl enable redis &>>$log_file
+systemctl restart redis &>>$log_file
+status_check_func $?
 
 
 
