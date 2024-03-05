b133d8ed4e86cfbd7e52cf27704f0bdce4307df6
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-03-05 12:01:18 +0530

    Functions

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -9,15 +9,23 @@ if [ -z "$rabbitmq_user_password" ]; then
 exit
    fi
 
-echo -e "\e[36m>>>>>>>>>>>>>>> Configure YUM Repos from the script provided by vendor. <<<<<<<<<\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-echo -e "\e[36m>>>>>>>>>>>>>>> Configure YUM Repos for RabbitMQ <<<<<<<<<\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-echo -e "\e[36m>>>>>>>>>>>>>>> Install RabbitMQ <<<<<<<<<\e[0m"
-dnf install rabbitmq-server -y
-echo -e "\e[36m>>>>>>>>>>>>>>> Adding app user and passwd forRabbitMQ Service<<<<<<<<<\e[0m"
-rabbitmqctl add_user roboshop ${rabbitmq_user_password}
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-echo -e "\e[36m>>>>>>>>>>>>>>> Start RabbitMQ Service<<<<<<<<<\e[0m"
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+print_head "Configure YUM Repos from the script provided by vendor"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_file
+status_check_func $?
+
+print_head " Configure YUM Repos for RabbitMQ"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
+status_check_func $?
+
+print_head "Install RabbitMQ"
+dnf install rabbitmq-server -y &>>$log_file
+status_check_func $?
+ print_head "Adding app user and passwd forRabbitMQ Service"
+ rabbitmqctl add_user roboshop ${rabbitmq_user_password} &>>$log_file
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
+status_check_func $?
+
+print_head "Start RabbitMQ Service"
+systemctl enable rabbitmq-server &>>$log_file
+systemctl restart rabbitmq-server &>>$log_file
+status_check_func $?
