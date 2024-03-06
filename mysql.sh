c03945d3a4fb8de60d0222de8b5a12466e9e2167
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-03-06 11:19:14 +0530

    Functions

--- mysql.sh
+++ mysql.sh
@@ -8,14 +8,23 @@ if [ -z "$mysql_root_password" ]; then
 exit
    fi
 
-echo -e "\e[36m>>>>>>>>>>>>>>>MySQL Disable<<<<<<<<<\e[0m"
-dnf module disable mysql -y
-echo -e "\e[36m>>>>>>>>>>>>>>>Copying MySQL repo file<<<<<<<<<\e[0m"
-cp ${script_path}/mysql.repo  /etc/yum.repos.d/mysql.repo
-echo -e "\e[36m>>>>>>>>>>>>>>>Install MySQL <<<<<<<<<\e[0m"
-dnf install mysql-community-server -y
-echo -e "\e[36m>>>>>>>>>>>>>>>Set root Passwd for MySQL <<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass ${mysql_root_password}
-echo -e "\e[36m>>>>>>>>>>>>>>>Start  MySQL <<<<<<<<<\e[0m"
+print_head "MySQL Disable"
+dnf module disable mysql -y &>>$log_file
+
+status_check_func $?
+
+print_head "Copying MySQL repo file"
+cp ${script_path}/mysql.repo  /etc/yum.repos.d/mysql.repo &>>$log_file
+
+status_check_func $?
+
+print_head"Install MySQL <<<<<<<<<\e[0m"
+dnf install mysql-community-server -y &>>$log_file
+status_check_func $?
+print_head "Set root Passwd for MySQL "
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$log_file
+status_check_func $?
+print_head  "Start  MySQL "
 systemctl enable mysqld
 systemctl restart mysqld
+status_check_func $?
