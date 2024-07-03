70f78a1e135103a4030aed2fdee4a81420b7e6cd
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-03 18:39:17 +0530

    Catalogue Service with Headings

--- redis.sh
+++ redis.sh
@@ -1,7 +1,9 @@
+echo -e "\e[36m>>>>>>>>>> Configuring redis repo file >>>>>>>>>>\e[0m "
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
+echo -e "\e[36m>>>>>>>>>> Install Redis>>>>>>>>>>\e[0m "
 dnf install redis -y
-# We need to Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
-
+sed -i -e  's|127.0.0.1|0.0.0.0|' /etc/redis.conf  /etc/redis/redis.conf
+echo -e "\e[36m>>>>>>>>>> Start Redis service >>>>>>>>>>\e[0m "
 systemctl enable redis
 systemctl restart redis
