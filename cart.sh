a3886dd08bab57238927201290b54b3915ca17de
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-03 19:15:03 +0530

    user service

--- cart.sh
+++ cart.sh
@@ -1,14 +1,23 @@
+echo -e "\e[36m>>>>>>>>>>Configuring Nodejsrepo>>>>>>>>>>\e[0m "
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
+echo -e "\e[36m>>>>>>>>>>Install Nodejs>>>>>>>>>>\e[0m "
 dnf install nodejs -y
+echo -e "\e[36m>>>>>>>>>>Add Application user >>>>>>>>>>\e[0m "
 useradd roboshop
+echo -e "\e[36m>>>>>>>>>>Creating App directory>>>>>>>>>>\e[0m "
+rm -rf /app
 mkdir /app
+echo -e "\e[36m>>>>>>>>>>Download APP content>>>>>>>>>>\e[0m "
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
+echo -e "\e[36m>>>>>>>>>>Unzip the Content >>>>>>>>>>\e[0m "
 unzip /tmp/cart.zip
-cd /app
+echo -e "\e[36m>>>>>>>>>>Install Nodejs Dependecies>>>>>>>>>>\e[0m "
 npm install
-cp cart.service /etc/systemd/system/cart.service
+echo -e "\e[36m>>>>>>>>>Copying Systemd Service file>>>>>>>>>>\e[0m "
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
+echo -e "\e[36m>>>>>>>>> Start Cart service>>>>>>>>>>\e[0m "
 systemctl daemon-reload
 
 systemctl enable cart
