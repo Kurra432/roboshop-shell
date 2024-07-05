47f70b26f1f451565c029290cdceea0592d1c392
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-05 10:14:45 +0530

    Mysql service

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,13 @@
 echo -e "\e[36m>>>>>>>>>>Configuring Mysql repo>>>>>>>>>>\e[0m "
 dnf module disable mysql -y
-cp msql.repo /etc/yum.repos.d/mysql.repo
+cp /home/centos/roboshop-shell/msql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[36m>>>>>>>>>>Install Mysql >>>>>>>>>>\e[0m "
 dnf install mysql-community-server -y
-echo -e "\e[36m>>>>>>>>>>Load Schema>>>>>>>>>>\e[0m "
-mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
+
 echo -e "\e[36m>>>>>>>>>>Start Mysql>>>>>>>>>>\e[0m "
 systemctl enable mysqld
 systemctl restart mysqld
+
+echo -e "\e[36m>>>>>>>>>>Load Schema>>>>>>>>>>\e[0m "
+mysql_secure_installation --set-root-pass RoboShop@1
+
