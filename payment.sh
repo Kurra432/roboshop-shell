8783f701681a5a5e6523346e2dd23375a5671d95
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-22 18:36:08 +0530

    Payment

--- payment.sh
+++ payment.sh
@@ -21,7 +21,7 @@ unzip /tmp/payment.zip
 echo -e "\e[36m>>>>>>>>>>>>Download the Dependecies <<<<<<<<<<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 echo -e "\e[36m>>>>>>>>>>Copying Systemd Service file>>>>>>>>>>\e[0m "
-sed -i -e "s|rabbitmq_app_password|${rabbitmq_app_password}" ${script_path}/payment.service
+sed -i -e 's|rabbitmq_app_password|${rabbitmq_app_password}|' ${script_path}/payment.service
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[36m>>>>>>>>>>>>Start Payment Service>>>>>>>>>>>>>\e[0m"
