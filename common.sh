e70d8cdc59f435a0aa71ca9e01ca4029e7232fa3
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-22 18:01:05 +0530

    python

--- common.sh
+++ common.sh
@@ -29,3 +29,29 @@ func_nodejs() {
 
 
 }
+
+
+func_python() {
+  echo -e "\e[36m>>>>>>>>>>Install Golang>>>>>>>>>>\e[0m "
+  dnf install golang -y
+  echo -e "\e[36m>>>>>>>>>>Add Application user>>>>>>>>>>\e[0m "
+  useradd ${app_user}
+  echo -e "\e[36m>>>>>>>>>>Add App Directory>>>>>>>>>>\e[0m "
+  rm -rf /app
+  mkdir /app
+  echo -e "\e[36m>>>>>>>>>> Download App Content>>>>>>>>>>\e[0m "
+  curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+  cd /app
+  echo -e "\e[36m>>>>>>>>>> Unzip the Content>>>>>>>>>>\e[0m "
+  unzip /tmp/dispatch.zip
+  echo -e "\e[36m>>>>>>>>>> Setup the Golang>>>>>>>>>>\e[0m "
+  go mod init dispatch
+  go get
+  go build
+  echo -e "\e[36m>>>>>>>>>>Copying SystemD service file>>>>>>>>>>\e[0m "
+  cp ${script_path}/dispatch.service /etc/systemd/system/dispatch.service
+  echo -e "\e[36m>>>>>>>>>>Start Dispatch Service>>>>>>>>>>\e[0m "
+  systemctl daemon-reload
+  systemctl enable dispatch
+  systemctl restart dispatch
+}
--- payment.sh
+++ payment.sh
@@ -2,25 +2,12 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_app_password=$1
-echo -e "\e[36m>>>>>>>>>> Install Python>>>>>>>>>>\e[0m "
-dnf install python36 gcc python3-devel -y
-echo -e "\e[36m>>>>>>>>>> Add Application user>>>>>>>>>\e[0m "
-useradd ${app_user}
-echo -e "\e[36m>>>>>>>>>>Add App directory>>>>>>>>>>\e[0m "
-rm -rf /app
-mkdir /app
-echo -e "\e[36m>>>>>>>>>>Download App Content>>>>>>>>>>\e[0m "
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
-cd /app
-echo -e "\e[36m>>>>>>>>>>Unzip the Content>>>>>>>>>>\e[0m "
-unzip /tmp/payment.zip
-cd /app
-echo -e "\e[36m>>>>>>>>>>Download Dependencies>>>>>>>>>>\e[0m "
-pip3.6 install -r requirements.txt
-echo -e "\e[36m>>>>>>>>>> Copying SystemD service file>>>>>>>>>>\e[0m "
-sed -i -e "s|rabbitmq_app_password|${rabbitmq_app_password}|" ${script_path}/payment.service
-cp ${script_path}/payment.service /etc/systemd/system/payment.service
-echo -e "\e[36m>>>>>>>>>> Start Payment Service>>>>>>>>>>\e[0m "
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+
+if [ -z "$rabbitmq_app_password" ]; then
+  echo Input Missing
+  exit
+  fi
+
+component=payment
+
+func_python
