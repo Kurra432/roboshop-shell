532472213e59fed7a21a8608f8612b8cf634991a
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-23 11:34:07 +0530

    Rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,20 +1,30 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_app_password=$1
+rabbitmq_appuser_password=$1
 
-if [ -z "$rabbitmq_app_password" ]; then
-  echo Input Missing
-  exit
+if [ -z "$rabbitmq_appuser_password" ]; then
+  echo Input Roboshop Appuser Password Missing
+  exit 1
 fi
-echo -e "\e[36m>>>>>>>>>> Configuring Erlang  repos>>>>>>>>>>\e[0m "
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Setup ErLang Repos<<<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+
+ echo -e "\e[36m>>>>>>>>>>>>>>>>Setup RabbitMQ Repos<<<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-echo -e "\e[36m>>>>>>>>>>Install Rabbitmq>>>>>>>>>>\e[0m "
-dnf install rabbitmq-server -y
-echo -e "\e[36m>>>>>>>>>>Start rabbitmq server>>>>>>>>>>\e[0m "
+
+
+ echo -e "\e[36m>>>>>>>>>>>>>>>>Install ErLang & RabbitMQ <<<<<<<<<<<<<\e[0m"
+yum install rabbitmq-server -y
+
+
+ echo -e "\e[36m>>>>>>>>>>>>>>>>Start RabbitMQ Service<<<<<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server
-echo -e "\e[36m>>>>>>>>>> Add application user in Rabbitmq>>>>>>>>>>\e[0m "
-rabbitmqctl add_user roboshop ${rabbitmq_app_password}
+
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>Add Application User in RabbtiMQ<<<<<<<<<<<<<\e[0m"
+rabbitmqctl add_user roboshop ${rabbitmq_appuser_password}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
