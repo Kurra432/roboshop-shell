f630d8199e616f6d3f4a5af79462fe745b68ab12
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-25 11:07:53 +0530

    Catalogue

--- common.sh
+++ common.sh
@@ -21,7 +21,14 @@ func_schema_setup() {
  fi
 }
 
+if [ "$schema_setup" == "mysql" ]; then
 
+   print_head " Install Mysql"
+     dnf install mysql -y
+     print_head "load Schema"
+     mysql -h mysql-dev.vdevops72.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+
+fi
 
 
 func_nodejs() {
@@ -51,6 +58,34 @@ func_nodejs() {
   systemctl rstart ${component}
 
 func_schema_setup
+
+}
+
+
+func_java() {
+ print_head "Install Maven"
+   dnf install maven -y
+   print_head "Add Application User"
+   useradd ${app_user}
+  print_head "Create App Directory"
+   rm -rf /app
+   mkdir /app
+  print_head "Download App Content"
+   curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+   print_head "Unzip the Content"
+   cd /app
+   unzip /tmp/${component}.zip
+   print_head "Clean Maven Package"
+   mvn clean package
+   mv target/${component}-1.0.jar ${component}.jar
+   print_head "Copying Systemd Service file"
+   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+
+func_schema_setup
+ print_head "Start Shipping Service"
+   systemctl daemon-reload
+   systemctl enable ${component}
+   systemctl restart ${component}
 
 }
 
--- shipping.sh
+++ shipping.sh
@@ -2,28 +2,16 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
-echo -e "\e[36m>>>>>>>>>>Install Maven
-dnf install maven -y
-echo -e "\e[36m>>>>>>>>>>Add Application User
-useradd ${app_user}
-echo -e "\e[36m>>>>>>>>>>Create App Directory
-rm -rf /app
-mkdir /app
-echo -e "\e[36m>>>>>>>>>>Download App Content
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-echo -e "\e[36m>>>>>>>>>>Unzip the Content
-cd /app
-unzip /tmp/shipping.zip
-echo -e "\e[36m>>>>>>>>>>Clean Maven Package
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-echo -e "\e[36m>>>>>>>>>>Copying Systemd Service file
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-echo -e "\e[36m>>>>>>>>>> Install Mysql
-dnf install mysql -y
-mysql -h mysql-dev.vdevops72.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
 
-echo -e "\e[36m>>>>>>>>>>Start Shipping Service
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+if [ -z "$mysql_root_password" ]; then
+
+  echo Inputmisssing
+  exit
+  fi
+
+ component="shipping"
+
+schema_setup=mysql
+
+
+ func_java
