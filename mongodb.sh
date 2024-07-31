fbba89a09165a9c33661a0a7fa8c9b7071d96238
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-31 10:55:24 +0530

    Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+log_file=/tmp/roboshop.log
 
 print_head  "Copying SystemD service"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>log_file
@@ -10,7 +11,7 @@ print_head  "Install MongoDb"
 dnf install mongodb-org -y &>>log_file
  func_status_check $?
 
-print_head  "Modify the  MongoDblisten Adress"
+print_head  "Modify the MongoDblisten Adress"
 sed -i -e 's|127.0.0.1| 0.0.0.0|' /etc/mongod.conf &>>log_file
 func_status_check $?
 
