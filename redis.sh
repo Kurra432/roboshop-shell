3e3d040403696925a8dfa17cd6c255a00e312726
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-31 11:02:58 +0530

    Mongodb

--- redis.sh
+++ redis.sh
@@ -1,9 +1,22 @@
-echo -e "\e[36m>>>>>>>>>> Configuring redis repo file
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
-echo -e "\e[36m>>>>>>>>>> Install Redis
-dnf install redis -y
-sed -i -e  's|127.0.0.1|0.0.0.0|' /etc/redis.conf  /etc/redis/redis.conf
-echo -e "\e[36m>>>>>>>>>> Start Redis service
-systemctl enable redis
-systemctl restart redisssh
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+log_file=/tmp/roboshop.log
+
+print_head "Configuring redis repo file"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>log_file
+dnf module enable redis:remi-6.2 -y &>>log_file
+func_status_check $?
+
+print_head "Install Redis"
+dnf install redis -y &>>log_file
+func_status_check $?
+
+print_head "Edit the Listen address "
+sed -i -e  's|127.0.0.1|0.0.0.0|' /etc/redis.conf  /etc/redis/redis.conf &>>log_file
+func_status_check $?
+
+print_head "Start Redis service"
+systemctl enable redis &>>log_file
+systemctl restart redisssh &>>log_file
+func_status_check $?
