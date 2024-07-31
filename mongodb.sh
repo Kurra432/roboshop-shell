c397b75e0d896a9b2213b41f924ac46571079254
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-31 10:53:05 +0530

    Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,20 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-org -y
 
-sed -i -e 's|127.0.0.1| 0.0.0.0|' /etc/mongod.conf
+print_head  "Copying SystemD service"
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>log_file
+func_status_check $?
 
-systemctl enable mongod
-systemctl restart mongod
+print_head  "Install MongoDb"
+dnf install mongodb-org -y &>>log_file
+ func_status_check $?
+
+print_head  "Modify the  MongoDblisten Adress"
+sed -i -e 's|127.0.0.1| 0.0.0.0|' /etc/mongod.conf &>>log_file
+func_status_check $?
+
+print_head  "Start mongodb"
+systemctl enable mongod &>>log_file
+systemctl restart mongod &>>log_file
+func_status_check $?
