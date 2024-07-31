ca2d601b1ed1da04e009b9e32f22c1ad828cc137
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-07-31 10:32:13 +0530

    Application user

--- common.sh
+++ common.sh
@@ -75,11 +75,11 @@ func_apprequsites() {
 }
 
 func_systemd_setup() {
-    func_print_head "Setup SystemD Service"
+    print_head "Setup SystemD Service"
      cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
-     func_stat_check $?
+     func_status_check $?
 
-     func_print_head "Start ${component} Service"
+     print_head "Start ${component} Service"
      systemctl daemon-reload &>>$log_file
      systemctl enable ${component} &>>$log_file
      systemctl restart ${component} &>>$log_file
