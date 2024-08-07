572b98f650dc12912ff2bb47a318d248ebf75498
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-08-07 10:59:30 +0530

    Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ print_head "Setup MongoDB Repo"
 cp mongo.repo /etc/yum.repos.d/mongo.repo  &>>$log_file
 func_status_check $?
 
-print_head "Install MongoDB"
+print_head "Install Mongodb"
 yum install mongodb-org -y &>>$log_file
 func_status_check $?
 
@@ -14,7 +14,7 @@ print_head "Update MongoDB Listen Address"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
 func_status_check $?
 
-print_head "Start MongoDB"
+print_head "Start Mongodb"
 systemctl enable mongod &>>$log_file
 systemctl restart mongod &>>$log_file
 func_status_check $?
